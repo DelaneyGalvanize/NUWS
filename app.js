f5c8d3dc2d053d7a4ff9676f1485b72d29df8dc0
DelaneyGalvanize <delaney.patterson001@gmail.com>
2017-07-05 09:20:42 -0600

    heroku again

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+// app.use(express.static(path.join(__dirname, '/client/build')));
 // app.use(express.static(path.join(__dirname, './', 'node_modules')))
-
+app.use(express.static(path.resolve(__dirname, '/client/build')));
 
 app.use('/api/users', users);
 app.use('/api/tokens', tokens);
