86dc2d600c66de9f030f310d99d07ea957ef37b0
DelaneyGalvanize <delaney.patterson001@gmail.com>
2017-07-05 23:04:42 -0600

    splash page

--- client/src/components/splashPage.jsx
+++ client/src/components/splashPage.jsx
@@ -25,8 +25,9 @@ export default class SplashPage extends React.Component {
       <div className="box-content  box-2 box-style-2 box-shadow">
         <br></br>
         <br></br>
-        <h1 className='stat heads'> Welcome to Nuws. </h1>
-        <h2 className='stat heads'>Being Aware You May Find Yourself In A Dangerous Relationship Is A Great First Step.</h2>
+        <h1 className='stat heads'>Welcome to Nuws. We are glad you found us.</h1>
+        <h1 className='stat'> If your current relationship has shown signs of abuse, we want to help you get out.</h1>
+        {/* <h2 className='stat heads'>Being Aware You May Find Yourself In A Dangerous Situation With Your Partner Is A Great First Step.</h2> */}
         <div id="gradient" />
         <br></br>
         <br></br>
@@ -34,19 +35,20 @@ export default class SplashPage extends React.Component {
     </div>
       </div>
 
-  <div>
+  <div className='stats'>
     <Grid>
    <Row>
    <Col xs={6} md={6}>
      <Thumbnail className='splashstat'>
        <br></br>
-       <h3 className='stat'>Every minute, TWENTY people are victims of intimate partner violence</h3>
+       <h1 className='stat numero'>20</h1>
+       <h5 className='stat'>The number of people who are victims of intimate partner violence every MINUTE.</h5>
 
      </Thumbnail>
    </Col>
    <Col xs={6} md={6}>
-     <Thumbnail className='splashstat'>
-       <h1 className='stat'>1 in 4</h1>
+     <Thumbnail className='splashstat2'>
+       <h1 className='stat numero'>1 in 4</h1>
        <h5 className='stat'>The number of women who will be victims of severe violence by an intimate partner in their lifetimes.</h5>
 
      </Thumbnail>
@@ -57,15 +59,15 @@ export default class SplashPage extends React.Component {
 <Row>
 <Col xs={6} md={6}>
   <Thumbnail className='splashstat'>
-  <h1 className='stat'>THREE</h1>
+  <h1 className='stat numero'>THREE</h1>
     <h5 className='stat'>The number of women murdered every day by a current or former male partner in the U.S.</h5>
 
 
   </Thumbnail>
 </Col>
 <Col xs={6} md={6}>
-  <Thumbnail className='splashstat'>
-    <h1 className='stat'>4,774,000</h1>
+  <Thumbnail className='splashstat2'>
+    <h1 className='stat numero'>4,774,000</h1>
     <h5 className='stat'>The number of women in the U.S. who experience physical violence by an intimate partner every year.</h5>
 
 
@@ -80,7 +82,7 @@ export default class SplashPage extends React.Component {
 <div className="box-content box-2 container box-style-3 box-shadow">
   <br></br>
   <br></br>
-  <h1 className='stat'> If your current relationship has shown signs of abuse, we want to help you get out.</h1>
+  <h1 className='stat'> It is not too late. Check out the features below that Nuws offers to help you plan your escape!</h1>
   <div className="row">
       <br></br>
       <br></br>
@@ -89,28 +91,32 @@ export default class SplashPage extends React.Component {
 </div>
 </div>
 
+<br></br>
+<div className="container">
+    <h4 className='splash-heading'> Features </h4>
+</div>
   <div>
     <Grid>
       <Row >
       <Col xs={6} md={4}>
         <Thumbnail className="splash">
           <Glyphicon glyph="envelope"/>
-          <h3>Emergency Text</h3>
-          <p> You are allowed to enter two emergency contact numbers. When you double click the (black) bar at the top of the NUWS page a text with a message of your choice will be sent to your contacts immediately. Ex. "Come Get me At Home", "Call the Police"</p>
+          <h3 className='stat'>Emergency Text</h3>
+          <p className='stat'> Enter up to two emergency contact numbers and a message of your choice on the sign up page. At any time you need to reach out to a contact click the 'shop' button located within the 'Nordstrom Rack' picture on the right side. A message will be sent immediately. Example of message: "Come Get me At Home.", "Call the Police."</p>
         </Thumbnail>
       </Col>
       <Col xs={6} md={4}>
         <Thumbnail className="splash">
           <Glyphicon glyph="th-list" />
-          <h3>Escape Plan</h3>
-          <p>At the bottom of the NUWS page, a disguised 'Copyright' button that when clicked on will take you to a page that will help plan YOUR escape. Don't worry if you need to exit the page quickly, just hit the 'Save' button to immediately go back to the NUWS page!</p>
+          <h3 className='stat'>Escape Plan</h3>
+          <p className='stat'>At the bottom of the NUWS page, a disguised 'Copyright' button that when clicked on will take you to a page that will help plan YOUR escape. Don't worry if you need to exit the page quickly, just hit the 'Save' button at the bottom or top of page to immediately go back to the "NUWS" page!</p>
         </Thumbnail>
       </Col>
       <Col xs={6} md={4}>
         <Thumbnail className="splash">
           <Glyphicon glyph="heart" />
-          <h3>Recording</h3>
-          <p>The period "." in the NUWS logo on the "nuws" page is a secret recorder. At any moment you can click that button and begin recording a conversation that you can show the police or family member, etc. You have the option to save and delete the recording.</p>
+          <h3 className='stat'>Recording</h3>
+          <p className='stat'>The word 'styles' located in the NUWS toolbar is a secret recorder. At any moment you can click that button and begin recording a conversation that you can show the police or family member, etc. You have the option to play,save and delete the recording.</p>
         </Thumbnail>
       </Col>
       </Row>
